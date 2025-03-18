1bf487c08e30ac8205b77fe03a52095b2865f695
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2025-03-18 12:51:31 +0200

    FIX accordion content 1 grid

--- frontend/src/components/Accordion/AccordContent/AccordContent1.jsx
+++ frontend/src/components/Accordion/AccordContent/AccordContent1.jsx
@@ -18,8 +18,8 @@ const AccordContent1 = ({
 }) => {
 	const img_path = ''
 	return (
-		<div className='p-5'>
-			<div className='row flex'>
+		<>
+			<div className='grid grid-cols-12 lg:grid-rows-5 gap-x-3 max-lg:grid-cols-2 p-5'>
 				<img
 					src={
 						img_path.length === 0
@@ -27,83 +27,74 @@ const AccordContent1 = ({
 							: user_img_path
 					}
 					alt='...'
-					className='ac-profile-image p-1 mr-4'
+					className='ac-profile-image row-span-2 col-span-2 max-lg:col-span-2'
 				/>
-				<div className='flex flex-col gap-1 w-full'>
-					<div className='flex gap-3'>
-						<div className='flex flex-col w-2/9'>
-							<label className='ac-input-labels'>Ваш ID:</label>
-							<input
-								type='text'
-								className='ac-input w-full p-2'
-								value={user_id}
-							/>
-						</div>
-						<div className='flex flex-col w-3/9'>
-							<label className='ac-input-labels'>Электронная почта</label>
-							<input
-								type='text'
-								className='ac-input w-full p-2'
-								value={user_email}
-							/>
-						</div>
-						<div className='flex flex-col w-4/9'>
-							<label className='ac-input-labels'>Номер телефона</label>
-							<input
-								type='text'
-								className='ac-input w-full p-2'
-								value={user_phone}
-							/>
-						</div>
-					</div>
 
-					<div className='flex gap-3'>
-						<div className='flex flex-col w-3/9'>
-							<label className='ac-input-labels'>Фамилия</label>
-							<input
-								type='text'
-								className='ac-input w-full p-2'
-								value={user_surname}
-							/>
-						</div>
-						<div className='flex flex-col w-2/9'>
-							<label className='ac-input-labels'>Имя</label>
-							<input
-								type='text'
-								className='ac-input w-full p-2'
-								value={user_name}
-							/>
-						</div>
-						<div className='flex flex-col w-4/9'>
-							<label className='ac-input-labels'>Отчество</label>
-							<input
-								type='text'
-								className='ac-input w-full p-2'
-								value={user_patronymic}
-							/>
-						</div>
-					</div>
+				<div className='row-span-1 col-span-2  max-lg:col-span-1 max-lg:order-1'>
+					<label className='text-sm'>Ваш ID:</label>
+					<input type='text' className='ac-input w-full p-2' value={user_id} />
 				</div>
-			</div>
-			<div className='row flex gap-3'>
-				<div className='flex flex-col w-1/3'>
-					<label className='ac-input-labels'>Дата рождения</label>
+				<div className='row-span-1 col-span-3  max-lg:col-span-2 max-lg:order-7'>
+					<label className='text-sm'>Электронная почта</label>
+					<input
+						type='text'
+						className='ac-input w-full p-2'
+						value={user_email}
+					/>
+				</div>
+				<div className='row-span-1 col-span-5  max-lg:col-span-1 max-lg:order-6'>
+					<label className='text-sm'>Номер телефона</label>
+					<input
+						type='text'
+						className='ac-input w-full p-2'
+						value={user_phone}
+					/>
+				</div>
+
+				<div className='row-span-1 col-span-3  max-lg:col-span-1 max-lg:order-3'>
+					<label className='text-sm'>Фамилия</label>
+					<input
+						type='text'
+						className='ac-input w-full p-2'
+						value={user_surname}
+					/>
+				</div>
+				<div className='row-span-1 col-span-2  max-lg:col-span-1 max-lg:order-2'>
+					<label className='text-sm'>Имя</label>
+					<input
+						type='text'
+						className='ac-input w-full p-2'
+						value={user_name}
+					/>
+				</div>
+				<div className='row-span-1 col-span-5  max-lg:col-span-1 max-lg:order-4'>
+					<label className='text-sm'>Отчество</label>
+					<input
+						type='text'
+						className='ac-input w-full p-2'
+						value={user_patronymic}
+					/>
+				</div>
+
+				<div className='row-span-1 col-span-4  max-lg:col-span-1 max-lg:order-5'>
+					<label className='text-sm'>Дата рождения</label>
 					<input
 						type='date'
 						className='ac-input w-full p-2'
 						value={user_birthday}
 					/>
 				</div>
-				<div className='flex flex-col w-1/3'>
-					<label className='ac-input-labels'>ИНН</label>
+				<div className='row-span-1 col-span-4  max-lg:col-span-1 max-lg:order-10'>
+					<label className='text-sm'>ИНН</label>
 					<input
 						type='password'
 						className='ac-input w-full p-2'
 						value={user_inn}
 					/>
 				</div>
-				<div className='relative flex flex-col w-1/3'>
-					<label className='ac-input-labels'>Форма подачи заявления</label>
+
+				<div className='relative row-span-1 col-span-4  max-lg:col-span-2 max-lg:order-8'>
+					<label className='text-sm'>Форма подачи заявления</label>
 					<div className='relative w-full'>
 						<select className='ac-input w-full p-2 pr-10 appearance-none'>
 							<option value='online'>Онлайн</option>
@@ -116,35 +107,33 @@ const AccordContent1 = ({
 						/>
 					</div>
 				</div>
-			</div>
-			<div className='row flex gap-3'>
-				<div className='flex flex-col w-1/3'>
-					<label className='ac-input-labels'>СНИЛС</label>
+
+				<div className='row-span-1 col-span-4  max-lg:col-span-1 max-lg:order-11'>
+					<label className='text-sm'>СНИЛС</label>
 					<input
 						type='text'
 						className='ac-input w-full p-2'
 						value={user_snils}
 					/>
 				</div>
-				<div className='flex flex-col w-1/3'>
-					<label className='ac-input-labels'>Дата СНИЛС</label>
+				<div className='row-span-1 col-span-4  max-lg:col-span-1 max-lg:order-12'>
+					<label className='text-sm'>Дата СНИЛС</label>
 					<input
 						type='date'
 						className='ac-input w-full p-2'
 						value={user_date_snils}
 					/>
 				</div>
-				<div className='flex flex-col w-1/3'>
-					<label className='ac-input-labels'>Копия СНИЛС</label>
-					<label className='ac-input-file w-55 text-center p-2'>
+				<div className='row-span-1 col-span-3 flex flex-col  max-lg:col-span-1 max-lg:order-13'>
+					<label className='text-sm'>Копия СНИЛС</label>
+					<label className='ac-input-file text-center p-2 my-1'>
 						Загрузить
 						<input type='file' className='hidden' />
 					</label>
 				</div>
-			</div>
-			<div className='row flex gap-3'>
-				<div className='relative flex flex-col w-2/3'>
-					<label className='ac-input-labels'>Гражданство</label>
+
+				<div className='relative row-span-1 col-span-8  max-lg:col-span-2 max-lg:order-14'>
+					<label className='text-sm'>Гражданство</label>
 					<div className='relative w-full'>
 						<select className='ac-input w-full p-2 pr-10 appearance-none'>
 							<option value='Russian'>Россия</option>
@@ -158,7 +147,7 @@ const AccordContent1 = ({
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
